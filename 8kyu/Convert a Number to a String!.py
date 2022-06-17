b648ed9b48f99261653b53b4889654b50d2dc4af
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-17 10:04:49 +0300

    Update Convert a Number to a String!.py

--- 8kyu/Convert a Number to a String!.py
+++ 8kyu/Convert a Number to a String!.py
@@ -1,6 +1,5 @@
-We need a function that can transform a number into a string.
-
-What ways of achieving this do you know?
+# We need a function that can transform a number into a string.
+# What ways of achieving this do you know?
 
 def number_to_string(num):
     return str(num)

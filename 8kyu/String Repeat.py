942f7eac6a89ee30b082dcb6df5e7a7e8bd08aa6
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-17 10:43:27 +0300

    Create String Repeat.py

---
+++ 8kyu/String Repeat.py
@@ -0,0 +1,4 @@
+#Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+def repeat_str(repeat, string):
+  return string * repeat

871d1a99124dcad3be2ffd16381c84fd875fd39f
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-17 10:05:06 +0300

    Update Convert a String to a Number!.py

--- 8kyu/Convert a String to a Number!.py
+++ 8kyu/Convert a String to a Number!.py
@@ -1,6 +1,5 @@
-We need a function that can transform a string into a number. What ways of achieving this do you know?
-
-Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+# We need a function that can transform a string into a number. What ways of achieving this do you know?
+# Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
 def string_to_number(s):
     return int(s)

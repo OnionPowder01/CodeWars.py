c75153b088ccf59b7754411be2c9cd3e22f9f628
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-17 09:16:32 +0300

    Create Fake Binary.py

---
+++ 8kyu/Fake Binary.py
@@ -0,0 +1,12 @@
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+def fake_bin(x):
+    result = ""
+    for num in x:
+        if int(num) < 5:
+            result = result + "0"
+        else:
+            result = result + "1"
+    return result

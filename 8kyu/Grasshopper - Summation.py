a51c3989b115eff89db87c7ee0b260a82dbb3272
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-18 12:08:51 +0300

    Create Grasshopper - Summation.py

---
+++ 8kyu/Grasshopper - Summation.py
@@ -0,0 +1,9 @@
+#Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+def summation(num):
+    total = []
+    for i in range(1, num + 1):
+        total.append(i)
+    return sum(total)
+    
+

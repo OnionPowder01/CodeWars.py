4e249029953ffdb6bf64c4696d1f26ec9f22dd18
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-18 13:24:39 +0300

    Create Century From Year.py

---
+++ 8kyu/Century From Year.py
@@ -0,0 +1,7 @@
+#The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+#Given a year, return the century it is in.
+
+from math import 
+
+def century(year):
+    return ceil(year / 100)

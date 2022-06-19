c68e4a2d0a00d82b86adb457788053ecba277e20
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-19 15:46:21 +0300

    Create Returning Strings.py

---
+++ 8kyu/Returning Strings.py
@@ -0,0 +1,6 @@
+#Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+    

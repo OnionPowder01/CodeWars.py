e674c3291980d22f9ef75d84d42e5d35f1efbc74
OnionPowder01 <106592392+OnionPowder01@users.noreply.github.com>
2022-06-20 19:50:04 +0300

    Create Beginner - Lost Without a Map.py

---
+++ 8kyu/Beginner - Lost Without a Map.py
@@ -0,0 +1,12 @@
+''' Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6] ''' 
+
+def maps(a):
+    total = []
+    for i in a:
+        total.append(i * 2)
+    return total
+    
